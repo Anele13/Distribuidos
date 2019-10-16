4da21b5f1801bdf3962bc308c87850ed569394d4
anele <anelegaribaldi@gmail.com>
2019-10-15 22:59:41 -0300

    agregada alta, busqueda y falta un poco de modificacion y dockerfile

--- tp3/punto1/cgiserver.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
-from http.server import HTTPServer, CGIHTTPRequestHandler
-
-class Handler(CGIHTTPRequestHandler):
-    cgi_directories = ["."]
-
-httpd = HTTPServer(("", 8000), Handler)
-httpd.serve_forever()   
