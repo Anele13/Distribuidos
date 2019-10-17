d7daf0193617f99f37f2a760e581b13e8e4367c9
matias <matias.ducid08@gmail.com>
2019-10-17 19:11:20 -0300

    empezando punto 4 merge

--- tp3/punto1/scripts/responses_html/response_html.py
+++ tp3/punto1/scripts/responses_html/response_html.py
@@ -54,6 +54,28 @@ class ResponseHtml():
                             </html>"
         self.setContentBody(cadena)
 
+#Funcion para el punto 4.
+    def http_response_rango_edades():
+        cadena1 ="<link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T' crossorigin='anonymous'>\
+            <script src='https://code.jquery.com/jquery-3.3.1.slim.min.js' integrity='sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo' crossorigin='anonymous'></script>\
+            <script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js' integrity='sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1' crossorigin='anonymous'></script>\
+            <script src='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js' integrity='sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM' crossorigin='anonymous'></script>\
+            <html>\
+            <meta charset='utf-8'>"    
+        cadena2 = "<div class='row justify-content-center'>\
+                '<form action='consulta_rango_edades' method='POST' name='consulta_rango_edades_form' required>\
+                    <select name='select_rango' id='select_rango' placeholder='rango_edades'>\
+                        <option value='1'>0-20</option>\
+                        <option value='2'>20-40</option>\
+                        <option value='3'>mas de 40</option>\
+                    </select>\
+                    <button class='btn-default' type='submit'>Buscar</button>'\
+                </form>\
+            </div>\
+        </html>"
+        self.setContentBody(cadena1 + cadena2)
+
+
     def setHeader(self, new_header):
         self.header = new_header
             
@@ -87,22 +109,3 @@ class ResponseHtml():
 
 
 
-    def http_response_rango_edades():
-        cadena1 ="<link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T' crossorigin='anonymous'>\
-            <script src='https://code.jquery.com/jquery-3.3.1.slim.min.js' integrity='sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo' crossorigin='anonymous'></script>\
-            <script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js' integrity='sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1' crossorigin='anonymous'></script>\
-            <script src='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js' integrity='sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM' crossorigin='anonymous'></script>\
-            <html>\
-            <meta charset='utf-8'>"    
-        cadena2 = "<div class='row justify-content-center'>\
-                '<form action='consulta_rango_edades' method='POST' name='consulta_rango_edades_form' required>\
-                    <select name='select_rango' id='select_rango' placeholder='rango_edades'>\
-                        <option value='1'>0-20</option>\
-                        <option value='2'>20-40</option>\
-                        <option value='3'>mas de 40</option>\
-                    </select>\
-                    <button class='btn-default' type='submit'>Buscar</button>'\
-                </form>\
-            </div>\
-        </html>"
-        self.setContentBody(cadena1 + cadena2)
