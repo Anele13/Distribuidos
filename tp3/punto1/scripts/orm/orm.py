6d5eb4ef06c783f34277264a1dbbcbe1ac99cb5e
anele <anelegaribaldi@gmail.com>
2019-10-18 13:41:35 -0300

    ultimos cambios

--- tp3/punto1/scripts/orm/orm.py
+++ tp3/punto1/scripts/orm/orm.py
@@ -1,15 +1,73 @@
 import csv
+from tempfile import NamedTemporaryFile
+import shutil
 
 class Orm():
     """
     Clase para manejar las consulta al archivo usuarios.csv 
     """
+    cookies_filepath = '/usr/local/apache2/cookies.csv'
+    usuarios_filepath = '/usr/local/apache2/usuarios.csv'
 
     @classmethod
     def modificateUser(self,legajo, form):
+        nya = form.getvalue("nya")
+        sexo = form.getvalue("select_sexo")
+        edad = form.getvalue("edad")
+        pwd = form.getvalue("pwd")
 
-
+        #--------------------------------------------------
+        # Busco todos los usuarios en el archivo de cookies
+        cookie_user_list = []
+        with open(self.cookies_filepath, 'r') as csvFile:
+            reader = csv.reader(csvFile)
+            for row in reader:
+                cookie_user_list.append(row)
+        csvFile.close()
+        # Actualizo solo el que necesito
+        with open(self.cookies_filepath, 'w') as csvFile:
+            writer = csv.writer(csvFile)
+            for usuario in cookie_user_list:
+                if usuario[0] == legajo:
+                    writer.writerow([usuario[0], pwd, usuario[2]])
+                else:
+                    writer.writerow(usuario)
+        csvFile.close()
         
+        #---------------------------------------------------
+        # Busco todos los usuarios en el archivo de usuarios
+        user_list = []
+        with open(self.usuarios_filepath, 'r') as csvFile:
+            reader = csv.reader(csvFile)
+            for row in reader:
+                user_list.append(row)
+        csvFile.close()
+        # Actualizo solo el que necesito
+        with open(self.usuarios_filepath, 'w') as csvFile:
+            writer = csv.writer(csvFile)
+            for usuario in user_list:
+                if usuario[1] == legajo:
+                    writer.writerow([nya,usuario[1],sexo,edad,pwd])
+                else:
+                    writer.writerow(usuario)
+        csvFile.close()
+
+    @classmethod
+    def searchUserList(self, form):
+        """
+        Busca una lista de usuarios completa por legajo
+        """
+        legajo = form.getvalue("clave") #Busqueda por campo clave: Nombre de la persona
+        lista_usuarios_encontrados = []
+        with open('/usr/local/apache2/usuarios.csv' , 'r') as csvFile:
+            reader = csv.reader(csvFile)
+            for row in reader:
+                if legajo in row[1]:
+                    lista_usuarios_encontrados.append(row)
+        csvFile.close()
+        if not lista_usuarios_encontrados:
+            raise Exception("No se encontro ningun usuario con ese legajo.")
+        return lista_usuarios_encontrados
 
 
     @classmethod
@@ -43,9 +101,26 @@ class Orm():
 
 
     @classmethod
-    def createUser(self, lista_datos_usuario):
-        pass
-    
+    def createUser(self, form):
+        """
+        Metodo que sirve para dar de alta un nuevo usuario.
+        lanza una excepcion en caso de ingresar un legajo duplicado
+        """
+        nombre = form.getvalue("nya")
+        legajo = form.getvalue("legajo")
+        sexo = form.getvalue("select_sexo")
+        edad = form.getvalue("edad")
+        pwd = form.getvalue("pwd")
+
+        if self.searchUser(legajo):
+            raise Exception("El numero de legajo ingresado ya existe")
+        
+        row = [nombre, legajo, sexo, edad, pwd]
+        with open(self.usuarios_filepath, 'a') as csvFile:
+            writer = csv.writer(csvFile)
+            writer.writerow(row)
+        csvFile.close()
+        
 
     @classmethod
     def searchUser(self, legajo, pwd=None):
@@ -57,10 +132,10 @@ class Orm():
             for row in reader:
                 if pwd:
                     if legajo == row[1] and pwd == row[4]:
-                    return True
+                        return True
                 else:
                     if legajo == row[1]:
-                    return True
+                        return True
         return False
 
 
@@ -81,7 +156,7 @@ class Orm():
     @classmethod
     def saveTokenInTokenfile(self, legajo, pwd, token):
         """
-        Metodo que guarda en el archivo de tokens: legajo,pwd,token
+        Metodo que guarda en el archivo de tokens: legajo, pwd, token (EN ESE ORDEN)
         """
         row = [legajo,pwd, token]
         with open('/usr/local/apache2/cookies.csv', 'a') as csvFile:
--- tp3/punto1/scripts/responses_html/response_html.py
+++ tp3/punto1/scripts/responses_html/response_html.py
@@ -6,85 +6,166 @@ class ResponseHtml():
     """
     header = "Content-type: text/html"
     cookie = None
+    message = None
     body = None
 
-    def set_http_response_moficacion_usuario(self, datos_usuario):
-        # '<button class="btn btn-primary" type="submit" onclick='+'location.href="http://localhost:9090/index2.html"'+'>Volver</button>'+
-        cadena ="<link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T' crossorigin='anonymous'>\
-                <script src='https://code.jquery.com/jquery-3.3.1.slim.min.js' integrity='sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo' crossorigin='anonymous'></script>\
-                <script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js' integrity='sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1' crossorigin='anonymous'></script>\
-                <script src='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js' integrity='sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM' crossorigin='anonymous'></script>\
-                <html>\
-                <meta charset='utf-8'>"
+    def get_static(self):
+        """
+        metodo que tiene incluida las rutas a los estaticos bootstrap
+        """
+        static_path ="<link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T' crossorigin='anonymous'>\
+                    <script src='https://code.jquery.com/jquery-3.3.1.slim.min.js' integrity='sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo' crossorigin='anonymous'></script>\
+                    <script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js' integrity='sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1' crossorigin='anonymous'></script>\
+                    <script src='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js' integrity='sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM' crossorigin='anonymous'></script>"
+            
+        return static_path
+
+
+    def set_http_alert(self,message, level="default"):
+        """
+        Metodo que crea un mensaje de "Exito" o "Error" con Bootstrap luego de
+        realizar una operacion (alta usurio, modificacion, etc)
+        """
+        alert = "<h5><div align='center' class='alert alert-"+ level + " role='alert'>"+ message+ "</div></h5>"
+        self.message = alert
         
-        cadena2 = "<div class='row justify-content-center'>\
-                    '<form action='alta_usuario' method='POST' name='alta_usuario_form' required>\
-                        <input type='text' name='nya' id='nya' maxlength='70' placeholder='nombre y apellido' value="+datos_usuario[0]+" required>\
-                        <input type='number' name='legajo' id='legajo' maxlength='15' placeholder='Num. legajo' value="+datos_usuario[1]+" required>\
-                        <select name='select_sexo' id='select_sexo' placeholder='sexo'>\
-                            <option value='F'>Femenino</option>\
-                            <option value='M'>Masculino</option>\
-                        </select>\
-                        <input type='number' name='edad' id='edad' maxlength='10' placeholder='edad' value="+datos_usuario[3]+"required>\
-                        <input type='password' placeholder='pwd' name='pwd' id='pwd' value="+datos_usuario[4]+"required>\
-                        <button class='btn-default' type='submit' name='boton_submit_modificacion'>Modificar</button>'\
-                    </form>\
-                </div>\
-            </html>"
-        self.setContentBody(cadena+cadena2)
 
+    def set_http_response_busqueda_usuario(self, lista_usuarios=None):
+        cadena = ""
+        if lista_usuarios:
+            for usuario in lista_usuarios:
+                usuarios = usuarios + "<tr> <td>"+ usuario[0] +"</td> <td>"+ usuario[1] +"</td> <td>"+ usuario[2] +"</td> <td>"+ usuario[3] + "</td>  </tr>"
 
-    def set_http_response_login(self):
-        cadena = "<link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T' crossorigin='anonymous'>\
+                print("<link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T' crossorigin='anonymous'>\
                         <script src='https://code.jquery.com/jquery-3.3.1.slim.min.js' integrity='sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo' crossorigin='anonymous'></script>\
                         <script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js' integrity='sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1' crossorigin='anonymous'></script>\
                         <script src='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js' integrity='sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM' crossorigin='anonymous'></script>\
                         <html>\
-                        <meta charset='utf-8'>\
-                            <html>\
-                                <meta charset='utf-8'>\
-                                <button class='btn btn-primary' type='submit' onclick='location.href='index2.html''>Volver</button>\
-                                <div class='row justify-content-center'>\
-                                    <form action='login' method='POST' class='form-inline'>\
-                                        <input type='text' name='legajo' id='legajo' maxlength='70' placeholder='Numero de alumno (legajo)' required>\
-                                        <input type='text' name='pwd' id='pwd' maxlength='70' placeholder='Contraseña' required>\
-                                        <button class='btn btn-primary' type='submit'>Buscar Alumno</button>\
-                                    </form>\
-                                </div>\
-                            </html>"
+                        <meta charset='utf-8'>"+
+                        '<button class="btn btn-primary" type="submit" onclick='+'location.href="http://localhost:9090/index2.html"'+'>Volver</button>'+
+                        "<div class='row justify-content-center'>\
+                            <form action='busqueda_usuario' method='POST' name='alta_usuario_form' class='form-inline'>\
+                                <input type='text' name='clave' id='clave' maxlength='70' placeholder='inserte campo a buscar'>\
+                                <button class='btn btn-primary' type='submit'>Buscar</button>\
+                            </form>\
+                        </div>\
+                        <div class='row justify-content-center'>\
+                            <div class='col-md-4'>\
+                                <table class='table table-bordered'>\
+                                    <thead>\
+                                        <tr>\
+                                            <th>NyA</th>\
+                                            <th>Legajo</th>\
+                                            <th>Sexo</th>\
+                                            <th>Edad</th>\
+                                        </tr>\
+                                    </thead>\
+                                    <tbody>"
+                                    +usuarios+
+                                    "</tbody>\
+                                </table>\
+                            </div>\
+                        </div>\
+                    </html>")
+        else:
+            cadena = "<div class='row justify-content-center'>\
+                    <form action='busqueda_usuario' method='POST' class='form-inline'>\
+                        <input type='text' name='clave' id='clave' maxlength='70' placeholder='inserte campo a buscar' required>\
+                        <button class='btn btn-primary' type='submit'>Buscar</button>\
+                        </form>\
+                    </div>"
+        self.setContentBody(cadena)
+
+    def set_http_response_alta_usuario(self):
+        cadena = "<html>\
+                <meta charset='utf-8'>\
+                <button class='btn btn-primary' type='submit' onclick='location.href='index2.html''>Volver</button>\
+                <div class='row justify-content-center'>\
+                    <form action='alta_usuario' method='POST' name='alta_usuario_form' required>\
+                        <input type='text' name='nya' id='nya' maxlength='70' placeholder='nombre y apellido' required>\
+                    <input type='number' name='legajo' id='legajo' min='1' max='9999' placeholder='legajo' required>\
+                        <select name='select_sexo' id='select_sexo' placeholder='sexo'>\
+                            <option value='F'>Femenino</option>\
+                            <option value='M'>Masculino</option>\
+                        </select>\
+                        <input type='number' name='edad' id='edad' min='18' max='99' placeholder='edad' required>\
+                        <input type='password' placeholder='Contraseña' name='pwd' id='pwd' required>\
+                        <button class='btn-default' type='submit'>Alta</button>\
+                    </form>\
+                    </div> </html>"
+        self.setContentBody(cadena)
+
+
+    def set_http_response_moficacion_usuario(self, datos_usuario):
+        # '<button class="btn btn-primary" type="submit" onclick='+'location.href="http://localhost:9090/index2.html"'+'>Volver</button>'+
+        cadena =  "<html>\
+                    <meta charset='utf-8'>\
+                    <div class='row justify-content-center'>\
+                        '<form action='modificar_usuario' method='POST' name='alta_usuario_form' required>\
+                            <input type='text' name='nya' id='nya' maxlength='70' placeholder='nombre y apellido' value="+datos_usuario[0]+" required>\
+                            <select name='select_sexo' id='select_sexo' placeholder='sexo'>\
+                                <option value='F'>Femenino</option>\
+                                <option value='M'>Masculino</option>\
+                            </select>\
+                            <input type='number' name='edad' id='edad' maxlength='10' placeholder='edad' value="+datos_usuario[3]+" required>\
+                            <input type='password' placeholder='pwd' name='pwd' id='pwd' value="+datos_usuario[4]+" required>\
+                            <button class='btn-default' type='submit' value='submit' name='boton_submit_modificacion' id='boton_submit_modificacion'> Modificar </button>'\
+                        </form>\
+                    </div>\
+                </html>"
+        self.setContentBody(cadena)
+
+
+    def set_http_response_login(self):
+        cadena = "<html>\
+                    <meta charset='utf-8'>\
+                        <html>\
+                            <meta charset='utf-8'>\
+                            <button class='btn btn-primary' type='submit' onclick='location.href='index2.html''>Volver</button>\
+                            <div class='row justify-content-center'>\
+                                <form action='login' method='POST' class='form-inline'>\
+                                    <input type='text' name='legajo' id='legajo' maxlength='70' placeholder='Numero de alumno (legajo)' required>\
+                                    <input type='text' name='pwd' id='pwd' maxlength='70' placeholder='Contraseña' required>\
+                                    <button class='btn btn-primary' type='submit'>Buscar Alumno</button>\
+                                </form>\
+                            </div>\
+                        </html>"
         self.setContentBody(cadena)
 
     #Funcion para el punto 4.
-    def http_response_rango_edades():
-        cadena1 ="<link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T' crossorigin='anonymous'>\
-            <script src='https://code.jquery.com/jquery-3.3.1.slim.min.js' integrity='sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo' crossorigin='anonymous'></script>\
-            <script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js' integrity='sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1' crossorigin='anonymous'></script>\
-            <script src='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js' integrity='sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM' crossorigin='anonymous'></script>\
-            <html>\
-            <meta charset='utf-8'>"    
-        cadena2 = "<div class='row justify-content-center'>\
+    def set_http_response_rango_edades(self):
+        cadena = "<html>\
+                <meta charset='utf-8'>\
+                <div class='row justify-content-center'>\
                 '<form action='consulta_rango_edades' method='POST' name='consulta_rango_edades_form' required>\
                     <select name='select_rango' id='select_rango' placeholder='rango_edades'>\
                         <option value='1'>0-20</option>\
                         <option value='2'>20-40</option>\
                         <option value='3'>mas de 40</option>\
                     </select>\
-                    <button class='btn-default' type='submit'>Buscar</button>'\
-                </form>\
-            </div>\
-        </html>"
-        self.setContentBody(cadena1 + cadena2)
+                        <button class='btn-default' type='submit'>Buscar</button>'\
+                    </form>\
+                </div>\
+            </html>"
+        self.setContentBody(cadena)
+
+
+    def getContentBody(self):
+        return self.body
 
 
     def setHeader(self, new_header):
         self.header = new_header
             
+
     def setCookie(self, new_cookie):
         self.cookie = new_cookie
+
 
     def setContentBody(self, new_content):
         self.body = new_content
     
+
     def render(self):
         """
         Metodo para renderizar el html desde cualquier CGI
@@ -99,13 +180,19 @@ class ResponseHtml():
             print()
             print()
         # Body del html
+        print(self.get_static()) # path a estaticos bootstrap   
+        if (self.message):
+            print(self.message)
         print(self.body)
+
 
     def redirect(self, URI):
         print("Content-type: text/html\r")
         if self.cookie:
             print(self.cookie)
         print("Location: "+URI+"\r\n\r")
+        if self.message:
+            print(self.message)
 
 
 
