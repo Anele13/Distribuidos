bffa752e83f4f55f3b5f6672a5e60d547b4856eb
anele <anelegaribaldi@gmail.com>
2019-10-18 13:47:22 -0300

    ultimos cambios

--- tp3/punto1/scripts/responses_html/response_html.py
+++ tp3/punto1/scripts/responses_html/response_html.py
@@ -32,41 +32,31 @@ class ResponseHtml():
 
     def set_http_response_busqueda_usuario(self, lista_usuarios=None):
         cadena = ""
+        usuarios = ""
         if lista_usuarios:
             for usuario in lista_usuarios:
                 usuarios = usuarios + "<tr> <td>"+ usuario[0] +"</td> <td>"+ usuario[1] +"</td> <td>"+ usuario[2] +"</td> <td>"+ usuario[3] + "</td>  </tr>"
 
-                print("<link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T' crossorigin='anonymous'>\
-                        <script src='https://code.jquery.com/jquery-3.3.1.slim.min.js' integrity='sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo' crossorigin='anonymous'></script>\
-                        <script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js' integrity='sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1' crossorigin='anonymous'></script>\
-                        <script src='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js' integrity='sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM' crossorigin='anonymous'></script>\
-                        <html>\
-                        <meta charset='utf-8'>"+
-                        '<button class="btn btn-primary" type="submit" onclick='+'location.href="http://localhost:9090/index2.html"'+'>Volver</button>'+
-                        "<div class='row justify-content-center'>\
-                            <form action='busqueda_usuario' method='POST' name='alta_usuario_form' class='form-inline'>\
-                                <input type='text' name='clave' id='clave' maxlength='70' placeholder='inserte campo a buscar'>\
-                                <button class='btn btn-primary' type='submit'>Buscar</button>\
-                            </form>\
-                        </div>\
-                        <div class='row justify-content-center'>\
-                            <div class='col-md-4'>\
-                                <table class='table table-bordered'>\
-                                    <thead>\
-                                        <tr>\
-                                            <th>NyA</th>\
-                                            <th>Legajo</th>\
-                                            <th>Sexo</th>\
-                                            <th>Edad</th>\
-                                        </tr>\
-                                    </thead>\
-                                    <tbody>"
-                                    +usuarios+
-                                    "</tbody>\
-                                </table>\
-                            </div>\
-                        </div>\
-                    </html>")
+            cadena="<html>\
+                    <meta charset='utf-8'>\
+                    <div class='row justify-content-center'>\
+                        <form action='busqueda_usuario' method='POST' name='alta_usuario_form' class='form-inline'>\
+                            <input type='text' name='clave' id='clave' maxlength='70' placeholder='inserte campo a buscar'>\
+                            <button class='btn btn-primary' type='submit'>Buscar</button>\
+                        </form>\
+                    </div>\
+                    <div class='row justify-content-center'>\
+                        <div class='col-md-4'>\
+                            <table class='table table-bordered'>\
+                                <thead>\
+                                    <tr>\
+                                        <th>NyA</th>\
+                                        <th>Legajo</th>\
+                                        <th>Sexo</th>\
+                                        <th>Edad</th>\
+                                    </tr>\
+                                </thead>\
+                                <tbody>"    +usuarios+    "</tbody>             </table>        </div>                    </div>     </html>"
         else:
             cadena = "<div class='row justify-content-center'>\
                     <form action='busqueda_usuario' method='POST' class='form-inline'>\
