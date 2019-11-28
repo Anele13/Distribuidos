b4980a7aa730593cc872ce2f4d2ae516c9f30527
matias <matias.ducid08@gmail.com>
2019-11-28 12:34:22 -0300

    permisos en 777

--- tp4/punto2/src/punto2/AgenteRecorredor.java
+++ tp4/punto2/src/punto2/AgenteRecorredor.java
@@ -57,17 +57,11 @@ public class AgenteRecorredor extends Agent
 		
 	    origen = here();
 		System.out.println("Origen --> " + origen.getName()+"\n");
-		
-		
-		
-
 		String[] columnas = {"Hora de arribo","Carga de procesamiento","IP","Sistema Operativo", "Versión SO", "Arquitectura SO","Cantidad de núcleos","Tamaño de memoria","Nombre de Plataforma Local","Vendor de Plataforma Local","Versión de Plataforma Local"};	
 		verContainers();
 		cantidad_maxima_contenedores = containers.size();
 		String[][] datos = new String[cantidad_maxima_contenedores][columnas.length];
 
-		
-				
 		// registra el comportamiento deseado del agente
 		addBehaviour( 
 			new CyclicBehaviour(this)
@@ -76,93 +70,33 @@ public class AgenteRecorredor extends Agent
 				{										
 					if (_state == 0)
 					{		
-						
-//							verContainers();
-						
-//						System.out.println("CONTAINERS : "+containers);
-//						eliminarMainContainer(origen.getID()); //[TODO]NO FUNCIONA!! NO ELIMINA EL CONTAINER!!
-//	            		System.out.println("CONTAINERS : "+containers);
 						cant_contenedores = 0;
 						if (cantidad_maxima_contenedores != 0)
 							_state = 1;
 						else
 							_state = 2;  
-						
 					}
-					contenedor_actual= here();
-//					System.out.println("TEST ---------------- CONTENEDOR ACTUAL = "+contenedor_actual.getName());
-					
-//					System.out.println("TEST ---------------- ESTADO ="+_state);
+//					contenedor_actual= here();
 					switch(_state)
 					{
 						case 1:
 							// ME MUDO A LA SIGUIENTE MAQUINA  
-//							System.out.println("Contenedor actual: "+cant_contenedores);
-//							System.out.println("Cantidad Máxima de contenedores: "+cantidad_maxima_contenedores);
 							if (cant_contenedores < cantidad_maxima_contenedores) {
 								destino = (Location)containers.get(cant_contenedores);
 								++cant_contenedores;
-							
 								System.out.println("Estado 1 Comienza la migracion del agente al destino --> " + destino.getName()+"\n");
 								try 
 								{
-//									System.out.println("TEST ----------------  me muevo a destino");
 									doMove(destino);
-//									if (destino.getName() == "Main-Container") {
-//										System.out.println("YA ESTOY EN EL ORIGEN PAPA");
-//									}
-//									else {				
-//										System.out.println("Me muevo a "+destino.getName());
-//										doMove(destino);
-//									}
-//									try
-//								    {
-//								    	System.out.println("Esperando 8 segundos ...\n");
-//								        Thread.sleep(8000);
-//								    }
-//								    catch(InterruptedException ex)
-//								    {
-//								        Thread.currentThread().interrupt();
-//								    }
-//									System.out.println("TEST ---------------- CONTENEDOR ACTUAL = "+contenedor_actual.getName());
-//									System.out.println("Despues de doMove en CyclicBehaviour de Estado 1 --> " + destino.getName()+"\n");
-//									System.out.println("TEST ---------------- Ya migre. Relevando información ...\n\n");
-									
-									//Mostramos info que requiere el punto:
-									//La Hora.
 									DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 									Date date = new Date();
-//									System.out.println("Hora actual: " + dateFormat.format(date));
-									
-									//La carga de procesamiento.
-								    OperatingSystemMXBean bean = (com.sun.management.OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
-//								    System.out.println("Carga de procesamiento: "+bean.getSystemLoadAverage());
-								    
-//								    //La IP.
+									OperatingSystemMXBean bean = (com.sun.management.OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
 								    try 
 								    {
 								    	String thisIp = InetAddress.getLocalHost().getHostAddress();
-//								    	Systedfvfagm.out.println("IP:"+thisIp);
-								    
-								    
-									    // Info adicional que nos interesa mostrar.
-//									    System.out.println("Sistema Operativo: ");
-//									    System.out.println("--> Tipo: "+bean.getName());
-//									    System.out.println("--> Versión: "+bean.getVersion());
-//									    System.out.println("--> Arquitectura: "+bean.getArch());
-//									    System.out.println("Hardware: ");
-//									    System.out.println("--> Cantidad de núcleos: "+bean.getAvailableProcessors());
-//									    System.out.println("--> Tamaño de memoria: "+bean.getTotalPhysicalMemorySize()+" bytes.");  
-									    RuntimeMXBean runtimeBean = ManagementFactory.getRuntimeMXBean();
-//									    System.out.println("Plataforma local:");
-//									    System.out.println("--> Nombre : "+runtimeBean.getVmName());
-//									    System.out.println("--> Vendor : "+runtimeBean.getVmVendor());
-//									    System.out.println("--> Versión : "+runtimeBean.getVmVersion());
-//									    
-//									    
+									    RuntimeMXBean runtimeBean = ManagementFactory.getRuntimeMXBean();									    
 									    String[] aux = {dateFormat.format(date),Double.toString(bean.getSystemLoadAverage()),thisIp,bean.getName(),bean.getVersion(),bean.getArch(),Integer.toString(bean.getAvailableProcessors()),Long.toString(bean.getTotalPhysicalMemorySize()),runtimeBean.getVmName(),runtimeBean.getVmVendor(),runtimeBean.getVmVersion()};
 									    datos[auxiliar]=aux;
-									//Espera del tiempo solicitado.
 								    }
 								    catch(Exception e) 
 								    {
@@ -197,17 +131,9 @@ public class AgenteRecorredor extends Agent
 								break;
 							}
 						case 2:	
-							
-//							System.out.println("TEST ---------------- EMPIEZA EL CASO 2 +++++++++++++++");
-//							System.out.println("Estado 2 Comienza la auto eliminacion del agente en origen --> " + getName()+"\n");
-//							System.out.println("TEST ---------------- COMIENZA EL TRY");
 							try 
 							{
-//								System.out.println("TEST ---------------- REALIZO EL WAIT=");
-//								wait(1000);
-//								System.out.println("TEST ---------------- Voy a hacer el doDELETE");
 								doDelete();
-//								System.out.println("TEST ---------------- Hice el doDELETE");
 								System.out.println("Despues de la auto eliminacion del agente Estado 2 --> " + getName() +"\n");
 							} 
 							catch (Exception e) 
@@ -216,15 +142,12 @@ public class AgenteRecorredor extends Agent
 								e.getMessage();
 							}
 							//Imprimo la tabla al finalizar.
-
 							Tabla t = new Tabla(columnas, datos);
 							break; 
 					}
 				}
-
 				int auxiliar =0;
-				
-				private Location contenedor_actual=null;
+//				private Location contenedor_actual=null;
 				private int _state = 0; // variable de maquina de estados del agente
 			}
 		);
